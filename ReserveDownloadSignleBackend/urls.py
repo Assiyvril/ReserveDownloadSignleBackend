7d9f86e094502c40b6cbca437bbdd34b98645266
Assiyvril <zhangjingyu2999@gmail.com>
2024-02-04 13:37:52 +0800

    media Root

--- ReserveDownloadSignleBackend/urls.py
+++ ReserveDownloadSignleBackend/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from django.views.static import serve
+
+from ReserveDownloadSignleBackend import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
