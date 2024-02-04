73fe3a4d09392a53e38fa99b0f599bfd004803e3
Assiyvril <zhangjingyu2999@gmail.com>
2024-02-04 13:23:36 +0800

    服务配置

--- server_config/asgi.py
+++ server_config/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ReserveDownloadSignleBackend project.
+ASGI config for server_config project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ReserveDownloadSignleBackend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server_config.settings')
 
 application = get_asgi_application()
