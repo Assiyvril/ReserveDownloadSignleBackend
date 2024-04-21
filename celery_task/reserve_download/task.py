9c5788b1db4bdec369fa93440569360f34fbcaf9
Assiyvril <zhangjingyu2999@gmail.com>
2024-04-21 17:03:10 +0800

    上传excel 的 task

--- celery_task/reserve_download/task.py
+++ celery_task/reserve_download/task.py
@@ -3,7 +3,7 @@ from scripts_public import _setup_django
 import datetime
 from celery_task.celery_main import celery_app
 from reserve_download.models import ReserveDownload
-from reserve_download.scripts.inquire_order_info import ReserveDownloadOrderInquirer
+from reserve_download.scripts.inquire_order_info import ReserveDownloadOrderInquirer, OrderInquireByCode
 
 """
 预约下载
@@ -34,6 +34,39 @@ def scheduled_download_by_params(query_param_dict, reserve_download_record_id, f
         reserve_download_record_id=reserve_download_record_id,
         file_name=file_name,
     ).exec()
+    ReserveDownload.objects.filter(
+        id=reserve_download_record_id
+    ).update(
+        task_exec_end_time=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+    )
+
+
+@celery_app.task(name='reserve_download_by_upload_excel', soft_time_limit=SOFT_TIME_LIMIT, max_retries=MAX_RETRY_COUNT)
+def scheduled_download_by_upload_excel(parse_excel_data, available_fendian_id_list, order_code_mode, is_history, reserve_download_record_id, file_name):
+    """
+    定时下载 Task
+    定时下载
+    @param file_name:  文件名
+    @param reserve_download_record_id: 预约下载记录id
+    @param parse_excel_data: 解析的excel数据
+    @param available_fendian_id_list: 可用店铺id列表
+    @param order_code_mode: 订单号模式
+    @param is_history: 是否历史状态
+    """
+    ReserveDownload.objects.filter(
+        id=reserve_download_record_id
+    ).update(
+        task_exec_start_time=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+        task_status=3,
+    )
+    OrderInquireByCode(
+        parse_order_code_list=parse_excel_data,
+        available_fendian_id_list=available_fendian_id_list,
+        order_code_mode=order_code_mode,
+        is_history=is_history,
+        reserve_download_record_id=reserve_download_record_id,
+        file_name=file_name,
+    )
     ReserveDownload.objects.filter(
         id=reserve_download_record_id
     ).update(
